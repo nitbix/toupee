4cc3cee070fb1eca3ea7601ef702526f39545449
nitbix <nitbix@nitbix.com>
2015-09-16 18:26:27 +0100

    clean up examples

--- examples/load_ensemble.py
+++ examples/load_ensemble.py
@@ -1,15 +1,4 @@
 #!/usr/bin/python
-"""
-Run a MLP experiment from a yaml file
-
-Alan Mosca
-Department of Computer Science and Information Systems
-Birkbeck, University of London
-
-All code released under GPLv2.0 licensing.
-"""
-__docformat__ = 'restructedtext en'
-
 
 import gc
 import sys
@@ -17,30 +6,24 @@ import numpy as np
 import numpy.random
 import theano
 import theano.tensor as T
+import dill
 
-import mlp
-import config
-from logistic_sgd import LogisticRegression
-from data import *
+from toupee import config
+from toupee.data import *
 
 if __name__ == '__main__':
     params = config.load_parameters(sys.argv[1])
     dataset = load_data(params.dataset,
                               shared = False,
                               pickled = params.pickled)
-    x = T.matrix('x')
-    y = T.ivector('y')
+    members = dill.load(open(sys.argv[2]))
+    x = members[0].x
+    y = members[0].y
     method = params.method
-    method.prepare(dataset)
+    method.prepare(params,dataset)
     train_set = method.resampler.get_train()
     valid_set = method.resampler.get_valid()
-    members = []
-    for i in range(0,params.ensemble_size):
-        print 'training member {0}'.format(i)
-        new_member = method.create_member(x,y)
-        members.append(new_member)
-        gc.collect()
-    ensemble = method.create_aggregator(params,members,x,y,train_set,valid_set)
+    ensemble = params.method.create_aggregator(params,members,x,y,train_set,valid_set)
     test_set_x, test_set_y = method.resampler.get_test()
     test_model = theano.function(inputs=[],
         on_unused_input='warn',
