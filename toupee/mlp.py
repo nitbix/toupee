c1dc58ee3bc1d6d75cc1580ebb1dc912156c1f4e
Alan Mosca <nitbix@nitbix.com>
2015-11-28 20:54:43 +0000

    rename

--- toupee/mlp.py
+++ toupee/mlp.py
@@ -143,7 +143,7 @@ class MLP(object):
                 l.output_shape = self.chain_n_in
                 self.chain_in=l.output
                 return l
-            elif(layer_type == 'local_contrast_normalization'):
+            elif(layer_type == 'LCN'):
                 n_pixels_y,n_pixels_x = get_n_pixels()
                 kernel_size,use_divisor = desc
                 l = layers.LCN(
