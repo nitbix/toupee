37db0bdc28d919a5119207016b3ac99d98debfd1
Alan Mosca <nitbix@nitbix.com>
2016-05-17 01:00:17 +0100

    tidy up update rules and add ARMSProp2

--- toupee/update_rules.py
+++ toupee/update_rules.py
@@ -18,6 +18,10 @@ import common
 
 class UpdateRule(yaml.YAMLObject):
 
+    def _default_value(self, param_name, value):
+        if param_name not in self.__dict__:
+            self.__dict__[param_name] = value
+
     def __call__(self, param, learning_rate, gparam, mask, updates,
                  current_cost, previous_cost):
         raise NotImplementedError()
@@ -35,13 +39,13 @@ class SGD(UpdateRule):
         common.toupee_global_instance.add_reset_hook(lambda x: instance.reset(x))
         return instance
 
+    def set_defaults(self):
+        self._default_value('momentum',0.)
+        self._default_value('momentum_decay',1.)
+        self._default_value('renorm',False)
+
     def reset(self,updates):
-        if 'momentum' not in self.__dict__:
-            self.momentum = 0.
-        if 'momentum_decay' not in self.__dict__:
-            self.momentum_decay = 1.
-        if 'renorm' not in self.__dict__:
-            self.renorm = False
+        self.set_defaults()
         self.curr_momentum = sharedX(self.momentum)
 
     def epoch_hook(self,updates):
@@ -65,90 +69,8 @@ class SGD(UpdateRule):
         return new_w
 
     def serialize(self):
-        if 'momentum' not in self.__dict__:
-            self.momentum = 0
+        self.set_defaults()
         return 'SGD {{ momentum: {0} }}'.format(self.momentum)
-
-class FProp(UpdateRule):
-
-    yaml_tag = u'!FProp'
-
-    def __new__(cls):
-        instance = super(FProp,cls).__new__(cls)
-        common.toupee_global_instance.add_epoch_hook(lambda x: instance.epoch_hook(x))
-        common.toupee_global_instance.add_reset_hook(lambda x: instance.reset(x))
-        return instance
-
-    def reset(self,updates):
-        if 'momentum' not in self.__dict__:
-            self.momentum = 0.
-        if 'momentum_decay' not in self.__dict__:
-            self.momentum_decay = 1.
-        self.curr_momentum = sharedX(self.momentum)
-
-    def epoch_hook(self,updates):
-        updates.append((self.curr_momentum,self.curr_momentum * self.momentum_decay))
-
-    def __init__(self):
-        self.eta_plus  = 1.5
-        self.eta_minus = 0.25
-        self.max_delta = 50
-        self.min_delta = 1e-8
-
-    def __call__(self, param, learning_rate, gparam, mask, updates,
-                 current_cost, previous_cost):
-        ones_array = numpy.ones(param.shape.eval())
-        previous_grad = sharedX(ones_array,borrow=True)
-        delta = sharedX(self.min_delta * ones_array,borrow=True)
-        previous_inc = sharedX(numpy.zeros(param.shape.eval()),borrow=True)
-        zero = T.zeros_like(param)
-        one = T.ones_like(param)
-        change = previous_grad * gparam
-        q = sharedX(ones_array,borrow=True)
-        cost_increased = 0 #T.gt(current_cost,previous_cost)
-        masked_gparam = mask * gparam
-        masked =  T.eq(mask * gparam,0.)
-        change_above_zero = T.gt(change,0.)
-        change_below_zero = T.lt(change,0.)
-
-        def mask_filter(m,u):
-            return T.switch(masked, m, u)
-
-        new_delta = T.clip(
-                T.switch(
-                    change_above_zero,
-                    delta * self.eta_plus,
-                    T.switch(
-                        change_below_zero,
-                        delta * self.eta_minus,
-                        delta
-                    )
-                ),
-                self.min_delta,
-                self.max_delta
-        )
-        new_previous_grad = mask_filter(
-            previous_grad,
-            T.switch(change_below_zero, zero, gparam)
-        )
-
-        self.ms = sharedX(numpy.zeros(param.shape.eval()),borrow=True)
-        ms = 0.9 * self.ms + 0.1 * (gparam ** 2)
-        step = new_delta * gparam / T.sqrt(ms + 1.0e-11)
-
-        unmasked_inc = T.switch(change_below_zero, zero, step)
-        normal_inc = mask_filter(zero,unmasked_inc)
-
-        inc = T.switch(cost_increased, previous_inc, normal_inc)
-        d_w = T.switch(cost_increased, -previous_inc / (2 ** q), normal_inc)
-        new_q = T.switch(cost_increased, q + 1, 1)
-
-        updates.append((previous_grad,new_previous_grad))
-        updates.append((delta,new_delta))
-        updates.append((previous_inc,inc))
-        updates.append((q,new_q))
-        updates.append((self.ms,ms))
-        return param + d_w * mask
 
 
 class RMSProp(UpdateRule):
@@ -205,11 +127,12 @@ class Adam(UpdateRule):
         common.toupee_global_instance.add_reset_hook(lambda x: instance.reset(x))
         return instance
 
+    def set_defaults(self):
+        self._default_value('beta1',0.9)
+        self._default_value('beta2',0.999)
+
     def reset(self,updates):
-        if 'beta1' not in self.__dict__:
-            self.beta1 = 0.9
-        if 'beta2' not in self.__dict__:
-            self.beta2 = 0.999
+        self.set_defaults()
         self.t = 1
 
     def epoch_hook(self,updates):
@@ -233,28 +156,28 @@ class Adam(UpdateRule):
         return new_w
 
     def serialize(self):
-        if 'beta1' not in self.__dict__:
-            self.beta1 = 0.9
-        if 'beta2' not in self.__dict__:
-            self.beta2 = 0.999
+        self.set_defaults()
         return 'Adam {{ beta1: {0}, beta2: {1} }}'.format(self.beta1, self.beta2)
 
 
-class UProp(UpdateRule):
+class ARMSProp(UpdateRule):
 
-    yaml_tag = u'!UProp'
+    yaml_tag = u'!ARMSProp'
 
     def __new__(cls):
-        instance = super(UProp,cls).__new__(cls)
+        instance = super(ARMSProp,cls).__new__(cls)
         common.toupee_global_instance.add_epoch_hook(lambda x: instance.epoch_hook(x))
         common.toupee_global_instance.add_reset_hook(lambda x: instance.reset(x))
         return instance
+    
+    def set_defaults(self):
+        self._default_value('momentum',0.)
+        self._default_value('momentum_decay',1.)
+        self._default_value('eta_plus',1.1)
+        self._default_value('eta_minus',0.9)
 
     def reset(self,updates):
-        if 'momentum' not in self.__dict__:
-            self.momentum = 0.
-        if 'momentum_decay' not in self.__dict__:
-            self.momentum_decay = 1.
+        self.set_defaults()
         self.curr_momentum = sharedX(self.momentum)
 
     def epoch_hook(self,updates):
@@ -303,15 +226,95 @@ class UProp(UpdateRule):
         updates.append((self.velocity,velocity))
         updates.append((self.ms,ms))
         updates.append((self.acceleration,acceleration))
-        updates.append((self.acceleration_mean, acceleration))
+        updates.append((self.acceleration_mean,acceleration_mean))
         updates.append((previous_grad,gparam))
         updates.append((previous_delta_w,delta_w))
         return new_w
 
     def serialize(self):
-        if 'momentum' not in self.__dict__:
-            self.momentum = 0
-        return 'UProp {{ momentum: {0} }}'.format(self.momentum)
+        self.set_defaults()
+        return 'ARMSProp {{ momentum: {0} }}'.format(self.momentum)
+
+
+class ARMSProp2(UpdateRule):
+
+    yaml_tag = u'!ARMSProp2'
+
+    def __new__(cls):
+        instance = super(ARMSProp2,cls).__new__(cls)
+        common.toupee_global_instance.add_epoch_hook(lambda x: instance.epoch_hook(x))
+        common.toupee_global_instance.add_reset_hook(lambda x: instance.reset(x))
+        return instance
+
+    def set_defaults(self):
+        self._default_value('eta_plus',1.1)
+        self._default_value('eta_minus',0.9)
+        self._default_value('beta1',0.9)
+        self._default_value('beta2',0.999)
+
+    def reset(self,updates):
+        self.set_defaults()
+        self.t = 1
+
+
+    def epoch_hook(self,updates):
+        self.t += 1
+
+    def __init__(self):
+        self.eta_plus = 1.1
+        self.eta_minus = 0.9
+        self.beta1 = 0.9
+        self.beta2 = 0.9
+
+    def __call__(self, param, learning_rate, gparam, mask, updates,
+                 current_cost, previous_cost):
+        epsilon = 1.0e-11
+        self.m = sharedX(numpy.zeros(param.shape.eval()), borrow=True)
+        self.v = sharedX(numpy.zeros(param.shape.eval()), borrow=True)
+        self.velocity = sharedX(numpy.zeros(param.shape.eval()),borrow=True)
+        self.acceleration = sharedX(numpy.ones(param.shape.eval()),borrow=True)
+        self.acceleration_mean = sharedX(numpy.ones(param.shape.eval()),borrow=True)
+        self.ms = sharedX(numpy.zeros(param.shape.eval()),borrow=True)
+        previous_grad = sharedX(numpy.ones(param.shape.eval()),borrow=True)
+        previous_delta_w = sharedX(numpy.ones(param.shape.eval()),borrow=True)
+        zero = T.zeros_like(param)
+
+        change = previous_grad * gparam
+        change_below_zero = T.lt(change,0.)
+        change_above_zero = T.gt(change,0.)
+        cost_increased = T.gt(current_cost,previous_cost)
+
+        acceleration = T.switch(
+                        change_below_zero,
+                        self.acceleration * self.eta_minus,
+                        T.switch(change_above_zero,
+                            self.acceleration * self.eta_plus,
+                            self.acceleration
+                        )
+                       )
+        acceleration = T.clip(acceleration,0.1,10.0)
+        acceleration_mean = 0.9 * self.acceleration_mean + 0.1 * acceleration
+#        acceleration = theano.printing.Print("ACC")(acceleration)
+
+        m = self.beta1 * self.m + (1 - self.beta1) * gparam
+        v = self.beta2 * self.v + (1 - self.beta2) * (gparam ** 2)
+        m_ = m / (1 - (self.beta1 ** self.t))
+        v_ = v / (1 - (self.beta2 ** self.t))
+        scaled_delta_w = m_ / (T.sqrt(v_) + epsilon)
+        learning_rate = - learning_rate.get() * acceleration / acceleration_mean
+        delta_w = scaled_delta_w * mask * learning_rate
+        new_w = param + delta_w
+        updates.append((self.acceleration,acceleration))
+        updates.append((self.acceleration_mean,acceleration_mean))
+        updates.append((previous_grad,gparam))
+        updates.append((previous_delta_w,delta_w))
+        updates.append((self.m,m))
+        updates.append((self.v,v))
+        return new_w
+
+    def serialize(self):
+        self.set_defaults()
+        return 'ARMSProp2 {{ beta1: {0}, beta2:{1} }}'.format(self.beta1,self.beta2)
 
 
 class RPropVariant(UpdateRule):
