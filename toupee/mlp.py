463b73f3c95a1621d63306c2f7dcfe65dade80b1
Alan Mosca <nitbix@nitbix.com>
2016-06-27 04:06:02 +0100

    make the test shared too

--- toupee/mlp.py
+++ toupee/mlp.py
@@ -52,12 +52,15 @@ class DataHolder:
         self.orig_train_set_y = dataset[0][1]
         self.orig_valid_set_x = dataset[1][0]
         self.orig_valid_set_y = dataset[1][1]
+        self.orig_valid_set_x = dataset[2][0]
+        self.orig_valid_set_y = dataset[2][1]
         self.train_set_x = sharedX(self.orig_train_set_x, dtype=floatX)
         self.train_set_y = sharedX(self.orig_train_set_y, dtype='int32')
         self.valid_set_x = sharedX(self.orig_valid_set_x, dtype=floatX)
         self.valid_set_y = sharedX(self.orig_valid_set_y, dtype='int32')
         if len(dataset) > 2:
-            self.test_set_x, self.test_set_y = dataset[2]
+            self.test_set_x = sharedX(self.orig_test_set_x, dtype=floatX)
+            self.test_set_y = sharedX(self.orig_test_set_y, dtype='int32')
         else:
             self.test_set_x, self.test_set_y = (None,None)
     
