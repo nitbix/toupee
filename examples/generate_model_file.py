6aa47c1527cb4e91666913b8207018214ba00f18
Alan Mosca <nitbix@nitbix.com>
2016-09-14 22:08:45 +0100

    tool to make yaml models

---
+++ examples/generate_model_file.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+
+import keras
+
+from keras.datasets import cifar10
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, Flatten
+from keras.layers import Convolution2D, MaxPooling2D
+from keras.optimizers import SGD
+from keras.utils import np_utils
+
+input_shape = [3,32,32]
+output_classes = 10
+
+model = Sequential()
+
+model.add(Convolution2D(32, 3, 3, border_mode='same',
+                            input_shape=input_shape))
+model.add(Activation('relu'))
+model.add(Convolution2D(32, 3, 3))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Convolution2D(64, 3, 3, border_mode='same'))
+model.add(Activation('relu'))
+model.add(Convolution2D(64, 3, 3))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Flatten())
+model.add(Dense(512))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(output_classes))
+model.add(Activation('softmax'))
+
+print model.to_yaml()
