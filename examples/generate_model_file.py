f22423d532145fec195d39db38c225651ef89cfe
Alan Mosca <nitbix@nitbix.com>
2016-09-21 21:28:19 +0100

    correct models

--- examples/generate_model_file.py
+++ examples/generate_model_file.py
@@ -5,12 +5,13 @@ import keras
 from keras.datasets import cifar10
 from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential
-from keras.layers import Dense, Dropout, Activation, Flatten
-from keras.layers import Convolution2D, MaxPooling2D
+from keras.layers import Dense, Dropout, Activation, Flatten, ZeroPadding2D
+from keras.layers import Convolution2D, MaxPooling2D, AveragePooling2D
+from keras.layers.normalization import BatchNormalization
 from keras.optimizers import SGD
 from keras.utils import np_utils
 
-input_shape = [32,32,3]
+input_shape = [3,32,32]
 output_classes = 10
 
 model = Sequential()
@@ -36,5 +37,55 @@ model.add(Activation('relu'))
 model.add(Dropout(0.5))
 model.add(Dense(output_classes))
 model.add(Activation('softmax'))
+
+#all -cnn
+#model.add(ZeroPadding2D((1,1), input_shape=input_shape))
+#model.add(Convolution2D(96, 3, 3, border_mode='valid'))
+#model.add(BatchNormalization())
+#model.add(Activation('relu'))
+#model.add(ZeroPadding2D((1,1)))
+#model.add(Convolution2D(96, 3, 3, border_mode='valid'))
+#model.add(BatchNormalization())
+#model.add(Activation('relu'))
+#model.add(ZeroPadding2D((1,1)))
+#model.add(Convolution2D(96, 3, 3, subsample=(2,2)))
+#model.add(BatchNormalization())
+#model.add(Activation('relu'))
+#model.add(Dropout(0.5))
+#
+#model.add(ZeroPadding2D((1,1)))
+#model.add(Convolution2D(192, 3, 3, border_mode='valid'))
+#model.add(BatchNormalization())
+#model.add(Activation('relu'))
+#model.add(ZeroPadding2D((1,1)))
+#model.add(Convolution2D(192, 3, 3, border_mode='valid'))
+#model.add(BatchNormalization())
+#model.add(Activation('relu'))
+#model.add(ZeroPadding2D((1,1)))
+#model.add(Convolution2D(192, 3, 3, subsample=(2,2)))
+#model.add(BatchNormalization())
+#model.add(Activation('relu'))
+#model.add(Dropout(0.5))
+#
+#model.add(ZeroPadding2D((1,1)))
+#model.add(Convolution2D(192, 3, 3, border_mode='valid'))
+#model.add(BatchNormalization())
+#model.add(Activation('relu'))
+#model.add(Convolution2D(192, 1, 1, border_mode='valid'))
+#model.add(BatchNormalization())
+#model.add(Activation('relu'))
+#model.add(Convolution2D(10, 1, 1, border_mode='valid'))
+#model.add(BatchNormalization())
+#model.add(Activation('relu'))
+#
+#model.add(Flatten())
+##model.add(Dense(1024))
+##model.add(Activation('softmax'))
+#model.add(Dense(output_classes))
+#model.add(Activation('softmax'))
+#
+##model.add(Flatten())
+##model.add(AveragePooling2D((6,6)))
+#model.add(Activation('softmax'))
 
 print model.to_yaml()
