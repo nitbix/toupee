3b6fffbfb02329ebffe15453d6697c6bb2621cab
Alan Mosca <nitbix@nitbix.com>
2018-02-07 11:20:01 +0000

    add setup script

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+ 
+setup(
+    name='toupee',       # This is the name of your PyPI-package.
+    version='0.1',          # Update the version number for new releases
+    description='A library for Deep Learning ensembles, with a tooolkit for running experiments, based on Keras.',
+    url='http://github.com/nitbix/toupee',
+    author='Alan Mosca',
+    author_email='nitbix@nitbix.com',
+    license='MIT',
+    packages=['toupee']
+)
