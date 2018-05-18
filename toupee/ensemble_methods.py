cb696f73bfc1eaf5eb29593371d0b691ded1c6a3
Alan Mosca <alan@nplan.io>
2018-05-18 14:46:27 +0000

    refactor to avoid duplicate code

--- toupee/ensemble_methods.py
+++ toupee/ensemble_methods.py
@@ -31,11 +31,17 @@ class Aggregator:
     def compute(self, X):
         raise NotImplementedException()
 
-    def predict_proba(self, X):
+    def predict_proba(self, X=None):
         raise NotImplementedException()
 
     def predict_classes(self, X):
         return np.argmax(self.predict_proba(X), axis = 1)
+
+    def predict(self, X):
+        a = self.predict_proba(X)
+        m = np.argmax(a,axis=1)
+        return np.eye(out_shape[1])[m]
+
 
 
 class AveragingRunner(Aggregator):
@@ -48,34 +54,18 @@ class AveragingRunner(Aggregator):
         self.members = members
         self.wrapper = wrapper
 
-    def predict_proba(self, data):
+    def predict_proba(self, X):
         prob = []
         for (m_yaml, m_weights) in self.members:
             m = keras.models.model_from_yaml(m_yaml)
             m.set_weights(m_weights)
-            p = m.predict_proba(data, batch_size = self.params.batch_size)
+            p = m.predict_proba(X, batch_size = self.params.batch_size)
             if self.wrapper is not None:
                 p = self.wrapper(p)
             prob.append(p)
-            out_shape = m.layers[-1].output_shape
         prob_arr = np.array(prob)
         a = np.sum(prob_arr,axis=0) / float(len(self.members))
         return a
-
-    def predict(self, data):
-        prob = []
-        for (m_yaml, m_weights) in self.members:
-            m = keras.models.model_from_yaml(m_yaml)
-            m.set_weights(m_weights)
-            p = m.predict_proba(data, batch_size = self.params.batch_size)
-            if self.wrapper is not None:
-                p = self.wrapper(p)
-            prob.append(p)
-            out_shape = m.layers[-1].output_shape
-        prob_arr = np.array(prob)
-        a = np.sum(prob_arr,axis=0) / float(len(self.members))
-        m = np.argmax(a,axis=1)
-        return np.eye(out_shape[1])[m]
 
 
 class MajorityVotingRunner(Aggregator):
@@ -87,15 +77,12 @@ class MajorityVotingRunner(Aggregator):
         self.params = params
         self.members = members
 
-    def predict_proba(self,data):
-        return self.predict(data)
-
-    def predict(self,data):
+    def predict_proba(self,X):
         classifs = []
         for (m_yaml, m_weights) in self.members:
             m = keras.models.model_from_yaml(m_yaml)
             m.set_weights(m_weights)
-            c = m.predict(data, batch_size = self.params.batch_size)
+            c = m.predict(X, batch_size = self.params.batch_size)
             classifs.append(c)
             out_shape = m.layers[-1].output_shape
         classifs_arr = np.array(classifs)
@@ -115,32 +102,17 @@ class WeightedAveragingRunner(Aggregator):
         self.members = members
         self.weights = weights
 
-    def predict_proba(self,data):
+    def predict_proba(self,X):
         prob = []
         for i in range(len(self.members)):
             m_yaml, m_weights = self.members[i]
             m = keras.models.model_from_yaml(m_yaml)
             m.set_weights(m_weights)
-            p = m.predict_proba(data, batch_size = self.params.batch_size)
+            p = m.predict_proba(X, batch_size = self.params.batch_size)
             prob.append(p * self.weights[i])
-            out_shape = m.layers[-1].output_shape
         prob_arr = np.array(prob) / np.sum(self.weights)
         a = np.sum(prob_arr,axis=0) / float(len(self.members))
         return a
-        
-    def predict(self,data):
-        prob = []
-        for i in range(len(self.members)):
-            m_yaml, m_weights = self.members[i]
-            m = keras.models.model_from_yaml(m_yaml)
-            m.set_weights(m_weights)
-            p = m.predict_proba(data, batch_size = self.params.batch_size)
-            prob.append(p * self.weights[i])
-            out_shape = m.layers[-1].output_shape
-        prob_arr = np.array(prob) / np.sum(self.weights)
-        a = np.sum(prob_arr,axis=0) / float(len(self.members))
-        m = np.argmax(a,axis=1)
-        return np.eye(out_shape[1])[m]
         
         
 class WeightedAveragingRunner_Regression(Aggregator):
