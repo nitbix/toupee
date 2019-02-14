e63545820ea2d7d9d62ecee79a61a819b5ed734f
Alan Mosca <alan@nplan.io>
2019-02-14 14:56:56 +0000

    Make checkpointing optional

--- toupee/common.py
+++ toupee/common.py
@@ -35,11 +35,13 @@ class ModelCheckpointInMemory(Callback):
             be `min`, etc. In `auto` mode, the direction is
             automatically inferred from the name of the monitored quantity.
     '''
-    def __init__(self, monitor='val_loss', verbose=0, mode='auto'):
+    def __init__(self, monitor='val_loss', verbose=0, mode='auto',
+            save_best_only=False):
         super(ModelCheckpointInMemory, self).__init__()
         self.monitor = monitor
         self.verbose = verbose
-        
+        self.save_best_only = save_best_only
+
         self.best_model = h5py.File("/dev/null", driver = 'core',
                 backing_store = False)
         self.best_epoch = None
@@ -70,7 +72,7 @@ class ModelCheckpointInMemory(Callback):
             warnings.warn('Can save best model only with %s available, '
                           'skipping.' % (self.monitor), RuntimeWarning)
         else:
-            if self.monitor_op(current, self.best):
+            if self.monitor_op(current, self.best) or not self.save_best_only:
                 if self.verbose > 0:
                     print('Epoch %05d: %s improved from %0.5f to %0.5f,'
                           ' saving model'
@@ -82,9 +84,9 @@ class ModelCheckpointInMemory(Callback):
                 if self.verbose > 0:
                     print('Epoch %05d: %s did not improve' %
                           (epoch, self.monitor))
-                          
+
 class Toupee:
-    
+
     def __init__(self):
         self.reset()
 
@@ -154,8 +156,8 @@ def serialize(o):
             else:
                 raise Exception("don't know how to save {0}".format(type(o)))
 
-                
-#for classification problems:                
+
+#for classification problems:
 def errors(classifier, test_set_x, test_set_y):
     classification = classifier.predict_classes(test_set_x)
     c = numpy.argmax(test_set_y, axis=1)
@@ -165,10 +167,10 @@ def errors(classifier, test_set_x, test_set_y):
 def accuracy(classifier, test_set_x, test_set_y):
     e = errors(classifier, test_set_x, test_set_y)
     return 1.0 - (float(e.sum()) / float(test_set_y.shape[0]))
-    
-    
-    
-#for regression problems:   
+
+
+
+#for regression problems:
 def distance(predictor, test_set_x, test_set_y):
     #returns the distance squared (=MMSE)
     prediction = predictor.predict(test_set_x)
@@ -181,16 +183,14 @@ def euclidian_distance(predictor, test_set_x, test_set_y):
     euclidian_distance_squared = distance(predictor, test_set_x, test_set_y)
     euclidian_distance = numpy.sqrt(euclidian_distance_squared)
     return(numpy.sum(euclidian_distance) / float(test_set_y.shape[0]))
-    
+
 def relative_distance(predictor, test_set_x, test_set_y):
     #relative_distance = distance(y-y_pred) / sqrt(y^2)      [sqrt(y^2) = L2 norm]
     euclidian_distance_squared = distance(predictor, test_set_x, test_set_y)
     y_squared = numpy.sum(numpy.square(test_set_y), axis = 1)                   #both this and the previous line will need a sqrt, which can be done after the division
     relative_distance = numpy.sqrt(euclidian_distance_squared / y_squared)
     return(numpy.sum(relative_distance) / float(test_set_y.shape[0]))
-    
-    
-    
+
 
 if 'toupee_global_instance' not in locals():
     toupee_global_instance = Toupee()
