9d56aa427a2db4c48c42c626904abe49872af085
Alan Mosca <nitbix@nitbix.com>
2022-05-09 11:34:36 +0100

    mypy

--- toupee/ensembles/methods.py
+++ toupee/ensembles/methods.py
@@ -33,7 +33,7 @@ class EnsembleMethod:
         self.data = data
         self.size = size
         self.model_params = model_params
-        self.members: list[Any] = []
+        self.members: List[Any] = []
         self.aggregator = tp.ensembles.get_aggregator(aggregator)
         self.model_factory = model_factory
         self.wandb = wandb
